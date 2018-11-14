d6b50e6eb2b0e594a590846c5a1534a51869c8b7
Ganesh Velayathikkode Kizhakkekara <ganesh.kizhakkekara@ad.infosys.com>
2018-11-14 16:20:33 +0530

    Initial Commit

---
+++ src/main/java/com/textAssessment/base/TechonicalAssessemnt.java
@@ -0,0 +1,15 @@
+package com.textAssessment.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  
+public class TechonicalAssessemnt {
+
+
+	public static void main(String[] args) {
+		 SpringApplication.run(TechonicalAssessemnt.class, args);  
+
+	}
+	
+}
