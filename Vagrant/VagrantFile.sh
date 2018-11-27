370d09d747c003b034cf6eba0bfc108d72e784f6
GaneshKizhakkekara <45033789+GaneshKizhakkekara@users.noreply.github.com>
2018-11-27 23:45:10 +0530

    Update VagrantFile.sh

--- Vagrant/VagrantFile.sh
+++ Vagrant/VagrantFile.sh
@@ -9,7 +9,7 @@ Vagrant.configure(2) do |config|
 
 
   config.vm.define :database do |cfg|
-        cfg.vm.network :private_network, ip: "192.171.11.10"
+        cfg.vm.network :private_network, ip: "192.171.11.11"
         cfg.vm.provider :virtualbox do |v|
             v.name = "database"
         cfg.vm.provision :shell, :path => "database.sh"
@@ -17,7 +17,8 @@ Vagrant.configure(2) do |config|
     end
 
   config.vm.define :appvm do |cfg|
-        cfg.vm.network :private_network, ip: "192.171.11.11"
+        cfg.vm.network "forwarded_port", guest: 8888, host: 9898
+        cfg.vm.network :private_network, ip: "192.171.11.12"
         cfg.vm.provider :virtualbox do |v|
             v.name = "appvm"
         cfg.vm.provision :shell, :path => "appvm.sh"
@@ -25,12 +26,11 @@ Vagrant.configure(2) do |config|
     end
 
   config.vm.define :haproxy do |cfg|
-        cfg.vm.network :private_network, ip: "192.171.11.9"
+        cfg.vm.network :private_network, ip: "192.171.11.13"
         cfg.vm.provider :virtualbox do |v|
             v.name = "haproxy"
         cfg.vm.provision :shell, :path => "haproxy.sh"
-        cfg.vm.provision "file", source: "/root/Ganesh_ShortestPath/haproxy.cfg", destination: "/etc/haproxy/haproxy.cfg"
         cfg.vm.provision :shell, :path => "haproxystart.sh"
         end
     end
-end
+end1
